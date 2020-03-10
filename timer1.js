3469755b1430ec203aefe4685a3fabce883996ff
hammadNS96 <hammad96@outlook.com>
2020-03-10 17:23:06 +0000

    finished timer1

---
+++ timer1.js
@@ -0,0 +1,14 @@
+
+const alarms = process.argv.slice(2);
+
+for (let item of alarms) {
+  if (typeof (item) !== NaN) {
+    if (item >= 0)
+      setTimeout(() =>
+        process.stdout.write('\x07'), item * 1000);
+  }
+};
+
+
+
+
